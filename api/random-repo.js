e45384606b4d8c68c5f229b590e8e8b7cd57e3ae
Luis Miguel Sánchez <luis.miguel.sanve@gmail.com>
2025-06-03 22:37:30 +0200

    Added public repos

--- api/random-repo.js
+++ api/random-repo.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-  const repos = ["OAuth_API", "AnotherRepo", "CoolProject"];
+  const repos = ["OAuth_API", "RandomReposStat", "MouseDummy", "MouseDummy", "NetworkCheckerDaemon", "ShieldBadgeGenerator", "WineMenu", "ClamAVMenu", "PerformanceTrackerScript", "GGUF-to-PyTorchTensor", "GeminiPostSQL", "The_WinForms_of_Isaac", "GeminiPostSQL_ChatBot", "GeminiPostSQL_API", "WinFormsZOrderViewer", "FirebaseMessagingNotification", "CSharpPHP_API", "AutoUSBTetherScript", "JavaShopManagement", "AutoUpdaterAndroid", "MLP-Trainer-Examples", "TransformersNERTrainer", "UpgradeLinuxPackagesScript", "JavaCollectionMethods"];
   const randomRepo = repos[Math.floor(Math.random() * repos.length)];
 
   const imageUrl = `https://github-readme-stats.vercel.app/api/pin/?username=luismisanve&repo=${randomRepo}&theme=github_dark_dimmed&border_color=ffffff&border_radius=9`;
