fc1394ea0ab65229001b29f89f57587f567411fa
Luis Miguel Sánchez <luis.miguel.sanve@gmail.com>
2025-06-03 22:44:20 +0200

    Avoid Github to cache the dynamic image

--- api/random-repo.js
+++ api/random-repo.js
@@ -2,7 +2,12 @@ export default function handler(req, res) {
   const repos = ["OAuth_API", "RandomReposStat", "MouseDummy", "MouseDummy", "NetworkCheckerDaemon", "ShieldBadgeGenerator", "WineMenu", "ClamAVMenu", "PerformanceTrackerScript", "GGUF-to-PyTorchTensor", "GeminiPostSQL", "The_WinForms_of_Isaac", "GeminiPostSQL_ChatBot", "GeminiPostSQL_API", "WinFormsZOrderViewer", "FirebaseMessagingNotification", "CSharpPHP_API", "AutoUSBTetherScript", "JavaShopManagement", "AutoUpdaterAndroid", "MLP-Trainer-Examples", "TransformersNERTrainer", "UpgradeLinuxPackagesScript", "JavaCollectionMethods"];
   const randomRepo = repos[Math.floor(Math.random() * repos.length)];
 
-  const imageUrl = `https://github-readme-stats.vercel.app/api/pin/?username=luismisanve&repo=${randomRepo}&theme=github_dark_dimmed&border_color=ffffff&border_radius=9`;
+  // Add a cache-busting timestamp
+  const timestamp = Date.now();
+
+  const imageUrl = `https://github-readme-stats.vercel.app/api/pin/?username=luismisanve&repo=${randomRepo}&theme=github_dark_dimmed&border_color=ffffff&border_radius=9&cb=${timestamp}`;
+
+  res.setHeader('Cache-Control', 'no-store');
 
   res.redirect(imageUrl);
 }
