e0786b7eb3c90d8522e315de52decfea7484640b
Luis Miguel Sánchez <luis.miguel.sanve@gmail.com>
2025-06-04 08:05:29 +0200

    Update random-repo.js

--- api/random-repo.js
+++ api/random-repo.js
@@ -2,10 +2,7 @@ export default function handler(req, res) {
   const repos = ["OAuth_API", "RandomReposStat", "MouseDummy", "MouseDummy", "NetworkCheckerDaemon", "ShieldBadgeGenerator", "WineMenu", "ClamAVMenu", "PerformanceTrackerScript", "GGUF-to-PyTorchTensor", "GeminiPostSQL", "The_WinForms_of_Isaac", "GeminiPostSQL_ChatBot", "GeminiPostSQL_API", "WinFormsZOrderViewer", "FirebaseMessagingNotification", "CSharpPHP_API", "AutoUSBTetherScript", "JavaShopManagement", "AutoUpdaterAndroid", "MLP-Trainer-Examples", "TransformersNERTrainer", "UpgradeLinuxPackagesScript", "JavaCollectionMethods"];
   const randomRepo = repos[Math.floor(Math.random() * repos.length)];
 
-  // Add a cache-busting timestamp
-  const timestamp = Date.now();
-
-  const imageUrl = `https://github-readme-stats.vercel.app/api/pin/?username=luismisanve&repo=${randomRepo}&theme=github_dark_dimmed&border_color=ffffff&border_radius=9&cb=${timestamp}`;
+  const imageUrl = `https://github-readme-stats.vercel.app/api/pin/?username=luismisanve&repo=${randomRepo}&theme=github_dark_dimmed&border_color=ffffff&border_radius=9`;
 
   res.setHeader('Cache-Control', 'no-store');
 
